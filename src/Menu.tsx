56961c32c977a9c4169be448f8eb2ba1065af79b
Marian Steinbach <marian@giantswarm.io>
2021-10-28 15:05:44 +0200

    Add tools menu

---
+++ src/Menu.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//import './Menu.css';
+
+import { Button, Nav, Paragraph } from 'grommet';
+import { UserSettings } from 'grommet-icons';
+
+type MenuProps = {
+    endpointCount: number;
+};
+
+function Menu({ endpointCount }: MenuProps) {
+    return (
+        <Nav direction="row" pad="medium">
+            <Paragraph margin={{vertical: 'xsmall'}}>{ endpointCount } Endpoints</Paragraph>
+            <Button icon={<UserSettings/>} label="Settings" />
+        </Nav>
+    );
+}
+
+export default Menu;
