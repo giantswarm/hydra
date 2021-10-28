3a4716edea84a9b91c294ac420f3a02f94d7ba63
Marian Steinbach <marian@giantswarm.io>
2021-10-28 15:28:45 +0200

    Configure Router

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/" exact component={App} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
